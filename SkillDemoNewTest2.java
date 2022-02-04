449a097274927b0374fbea71dd05d0667bc592e9
vivin2709 <vivinvinil05@gmail.com>
2022-02-03 19:30:38 -0800

    error fixed

--- SkillDemoNewTest2.java
+++ SkillDemoNewTest2.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
  
 public class SkillDemoNewTest2 {
+    
    @Test
    public void subtraction() {
        assertEquals(8,SkillDemoNew2.subtract(6,10));
